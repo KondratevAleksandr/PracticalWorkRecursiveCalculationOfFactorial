3d3e366fdfcc02ea901f57fba14f2f69e8cf67e6
Kondratev Aleksandr <wisp.1991@gmail.com>
2024-10-21 08:08:57 +0500

    Task complete

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 4; // Вычисление факториала для числа 10
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = forkJoinPool.invoke(factorialTask);
+
+        System.out.println("Факториал " + n + "! = " + result);
+
+
+    }
+}
